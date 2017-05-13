a53f45e9e65f310aeaa58255c43e6184ac6885d7
Igor Andriushchenko <igor.segodnya@gmail.com>
2017-05-13 17:51:19 +0200

    Docs and usability improvements

--- CheckVmWareVCenter .ps1
+++ CheckVmWareVCenter .ps1
@@ -1,64 +1,69 @@
-function getUpdateDate {
-    param(
-        $machine,
-        $credential,
-        $server
-    ) 
+<#
+
+.SYNOPSIS
+Check all VMs in the remote VMWare vCenter and list those might now have WannaCry ransomware fix
+
+.DESCRIPTION
+This script connect to vCenter, finds all Windows machines that are powerd on and gets the last hotfix update date.
+The machine is reported as "under risk" in case if the latest update is older than 15.03.2017 - which means, it definitely
+has no WannaCry exploit patch installed.
+The final list of such VMs is saved to the file MACHINES_UNDER_RISK.txt in the current folder. 
+
+.PARAMETER ViServer
+Remote ViServer to connect to
+
+.PARAMETER Credential 
+Credentials for connecting to ViServer and accessing VMs
+
+#>
+
+param(
+    [string]$ViServer,
+    $Credential = (Get-Credential)
+)
+
+
+
+Get-Module -ListAvailable VMWare* | Import-Module
+
+Write-Host "Connecting to vSphere"
+$server = Connect-VIServer -Server $ViServer -Protocol https -Credential $Credential | Out-Null
+
+$patchDate = Get-Date -Date "2017-03-15T00:00:00.0000000" -F o 
+
+Write-Host "Getting Windows powered on machines"
+$allVms = Get-Vm | where { $_.PowerState -eq "PoweredOn" -and $_.ExtensionData.Guest.GuestFullName -like "*windows*" }
+
+$VmsUnderRisk = @()
+
+foreach ($vm in $allVms) {
+
 
     $command = "Get-Date -Date (Get-HotFix | sort InstalledOn -desc)[0].InstalledOn -Format o"
 
-    Write-Host "Checking machine: $($machine)"
+    Write-Host "Checking machine: $($vm)"
 
-    $date = Invoke-VMScript -VM $machine -GuestCredential $credential -ScriptType Powershell -ScriptText $command -Server $server
+    $updateDate = Invoke-VMScript -VM $vm -GuestCredential $Credential -ScriptType Powershell -ScriptText $command -Server $server
 
-    if ($date) {
-        return $date
+    if (-Not $updateDate) {
+        Write-Host "No info on hotfixes, considering just installed or unavailable for scan"
+        $updateDate = Get-Date -Format o
+    }
+
+    $updateDate = Get-Date -Date $updateDate -Format o
+
+    if ($updateDate -lt $currentDate)
+    {
+        Write-Host "Last update date is: $($updateDate)"
+        Write-Host "$($VM) under risk!!! :O"
+        $VmsUnderRisk += @($Vm.Name, $updateDate)
+        write-host $VmsUnderRisk
     }
     else {
-        Write-Host "No info on hotfixes, considering just installed or unavailable for scan"
-        return Get-Date -Format o
+            Write-Host "$($VM) is ok!"
     }
 
 }
 
-function CheckVmWareVCenter 
-{
-    param(
-        [string]$ViServer,
-        [string]$Vmhost,
-        $Credential = (Get-Credential)
-    )
+$VmsUnderRisk | Out-file -FilePath "MACHINES_UNDER_RISK.txt"
 
-    Get-Module -ListAvailable VMWare* | Import-Module
-
-    Write-Host "Connecting to vSphere"
-    $server = Connect-VIServer -Server $ViServer -Protocol https -Credential $Credential | Out-Null
-
-    $patchDate = Get-Date -Date "2017-03-15T00:00:00.0000000" -F o 
-
-    Write-Host "Getting Windows powered on machines"
-    $allVms = Get-Vm | where { $_.PowerState -eq "PoweredOn" -and $_.ExtensionData.Guest.GuestFullName -like "*windows*" }
-
-    $VmsUnderRisk = @()
-    
-    foreach ($vm in $allVms) {
-
-        $updateDate = getUpdateDate -machine $vm -isVirtual $true -credential $Credential -server $server    
-        $updateDate = Get-Date -Date $updateDate -Format o
-
-        if ($updateDate -lt $currentDate)
-        {
-            Write-Host "Last update date is: $($updateDate)"
-            Write-Host "$($VM) under risk!!! :O"
-            $VmsUnderRisk += @($Vm.Name, $updateDate)
-            write-host $VmsUnderRisk
-        }
-        else {
-             Write-Host "$($VM) is ok!"
-        }
-
-    }
-
-     $VmsUnderRisk | Out-file -FilePath "MACHINES_UNDER_RISK.txt"
-    
-}
